5d993d94a9e5940554e4db13afa5f1416baf64dc
CrimsonCoder42 <nostro37@gmail.com>
2022-08-15 12:54:31 -0400

    Partially Applied Functions

--- src/main/scala/HigherOrderFunctions.scala
+++ src/main/scala/HigherOrderFunctions.scala
@@ -1,3 +1,11 @@
-object HigherOrderFunctions {
+object HigherOrderFunctions extends App {
+
+  // take functions as arguments and are able to return function as result
+
+  def math(x: Double, y:Double, z: Double, f: (Double, Double) => Double): Double = f(f(x,y), z);
+
+  val result = math(50, 20, 10, (x,y) => x max y)
+
+  println(result)
 
 }

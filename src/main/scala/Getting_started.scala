5d6c770bb804363d8975a6c00ea2b4f9261ba700
CrimsonCoder42 <nostro37@gmail.com>
2022-08-15 12:08:21 -0400

    init commit

--- src/main/scala/Getting_started.scala
+++ src/main/scala/Getting_started.scala
@@ -1,3 +1,31 @@
-object Getting_started {
+object Getting_started extends App {
+
+  val age = 200;
+
+  age match {
+
+    case 20 => println(age);
+    case 18 => println(age);
+    case 19 => println(age);
+    case 21 => println(age);
+    case 30 => println(age);
+    case 40 => println(age);
+    case _ => println("default")
+
+  }
+
+  val result = age match {
+
+    case 20 => age;
+    case 18 => age;
+    case 19 => age;
+    case 21 => age;
+    case 30 => age;
+    case 40 => age;
+    case _ => age;
+
+  }
+  println(" result = " + result);
 
 }
+
